4f8dd1c932bf65d9c7976591160d45c7e5d81f90
Kelvin Lee <lijiarong2020@sinh.ac.cn>
2023-07-11 17:30:01 +0800

    Update LCP_definition.R

--- LCP_definition.R
+++ LCP_definition.R
@@ -6,7 +6,7 @@ malignant_spot <- filter(infercnv_label,CNVlabel=='Malignant') %>% rownames()
 malignant_type <- spot_type[malignant_spot,]
 sample_name <- gsub('_[ACTG]+-1','',rownames(malignant_type))
 ### remvoe NK, pDC, Mast cell for too low proportion
-malignant_type 《- malignant_type[,c('Parachymal cell','Fibroblast','Endothelial cell','CD4 T','CD8 T','B','Macrophage','cDC')]
+malignant_type <- malignant_type[,c('Parachymal cell','Fibroblast','Endothelial cell','CD4 T','CD8 T','B','Macrophage','cDC')]
 malignant_type <- apply(malignant_type,1,function(x){x/sum(x)}) %>% t()
 ### cutoff set to limit parachymal cell percentage
 spot_type_scaled <- malignant_type
