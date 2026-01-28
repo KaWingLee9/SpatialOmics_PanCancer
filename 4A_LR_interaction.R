21f11012ffe6c4ac86f54ba68b834d242d20fb0a
Kelvin Lee <lijiarong2020@sinh.ac.cn>
2026-01-29 00:47:29 +0800

    Update 4A_LR_interaction.R

--- 4A_LR_interaction.R
+++ 4A_LR_interaction.R
@@ -3,6 +3,7 @@ library(ggplot2)
 library(aplot)
 library(ComplexHeatmap)
 
+source('https://github.com/KaWingLee9/in_house_tools/blob/main/visulization/custom_fun.R')
 source('https://github.com/KaWingLee9/in_house_tools/blob/main/meta_analysis/custom_fun.R')
 
 # LR interaction score of spots
@@ -178,4 +179,127 @@ p=OrderedPlot(p,x='pathway_name',y='Niche',cluster_value='OR1',cluster_var=NULL,
               cluster_column=TRUE,show_column_dend=TRUE,
               method='ward.D2'
               )
+p
+
+# showing the expression of ligands and receptors respectively
+
+# meta analysis
+for (dataset in dataset_used){
+
+    print(dataset)
+
+    seurat_obj=readRDS( paste0('../1_data_preprocessing/',dataset,'/',dataset,'.rds') )
+    
+    ind=intersect(colnames(seurat_obj),rownames(niche_cluster_result))
+    seurat_obj=seurat_obj[,ind]
+    
+    Idents(seurat_obj)=niche_cluster_result[colnames(seurat_obj),'Niche_combined']
+    
+    x=Idents(seurat_obj) %>% table()
+    
+    x=x[x>10] %>% names()
+    
+    # seurat_obj=subset(seurat_obj,idents=x)
+    
+    future::plan("multicore",workers=60) 
+    options(future.globals.maxSize=10000*1024^2)
+    sce.markers=FindAllMarkers(object=seurat_obj,
+                                only.pos=FALSE,min.pct=0,logfc.threshold=0,return.thresh=1)
+    
+    sce.markers[,'dataset']=dataset
+
+    sce.markers=filter(sce.markers,cluster %in% x)
+    
+    write.csv(sce.markers,paste0('./FindMarkers/',dataset,'.csv'))
+    
+}
+
+dir='./FindMarkers/'
+dataset_ls=gsub('.csv','',grep('BRCA|COAD|HNSC|CSCC|LUAD|GBM|OV|PRAD|PAAD|KIRC|LIHC|LUSC',dir(dir),value=TRUE))
+
+l=lapply(dataset_ls,function(x){
+
+    res=try({y <- read.csv(paste0(dir,x,'.csv'),row.names=1,check.names=FALSE)},silent=TRUE)
+    if (inherits(res,'try-error')) return(NULL)
+    return(y)
+})
+
+l=l[!unlist(lapply(l,is.null))]
+
+for (n in paste0('Niche_',1:13)){
+    
+    l_niche=parallel::mclapply(l,function(x){
+        y=filter(x,cluster==n)
+        rownames(y)=y[,'gene']
+        return(y)
+    },mc.cores=20) %>% .[lapply(.,nrow)!=0]
+
+    test_result=CombRank_DFLs(l_niche,p_col='p_val',ES_col='avg_log2FC',
+                              min_num=0,min_ratio=0.5,method='RankSum')
+
+    write.csv(test_result,paste0('./FindMarkers_meta/',n,'.csv'))
+    
+}
+
+meta_anlysis_result=lapply(dir('./FindMarkers_meta/'),function(x){
+    y=read.csv(paste0('./FindMarkers_meta/',x),row.names=1,check.names=FALSE)
+    y[,'Niche']=gsub('.csv','',x)
+    y[,'gene']=rownames(y)
+    return(y)
+}) %>% dplyr::bind_rows()
+
+rownames(meta_anlysis_result)=1:nrow(meta_anlysis_result)
+
+# showing the expression of ligands and receptors respectively
+df=meta_anlysis_result
+df[,'Niche']=factor(df[,'Niche'],c('Niche_1','Niche_2','Niche_3','Niche_4','Niche_5','Niche_6',
+                                   'Niche_7','Niche_8','Niche_9','Niche_10','Niche_11','Niche_12','Niche_13'))
+
+selected_chemokine_LR=c('CCL15-CCR1','CCL7-CCR1','CCL19-CCR7','CCL21-CCR7','CCL5-CCR1','CCL5-CCR3','CCL5-CCR4','CCL5-CCR5','CCL3-CCR1','CCL3-CCR5',
+                        'CXCL12-ACKR3','CXCL12-CXCR4','CXCL13-CXCR5','CXCL16-CXCR6','CXCL9-CXCR3','CXCL10-CXCR3','CXCL10-CXCR3')
+
+df_filter=df %>% filter(available_study>=1/3*total_study# ,sig_study_ratio>=0.3
+                       )
+
+LR_pairs=selected_chemokine_LR
+
+L=lapply(LR_pairs,function(x) {strsplit(x,'-')[[1]][1]  %>% strsplit(.,'[+]') %>% unlist()})
+R=lapply(LR_pairs,function(x) {strsplit(x,'-')[[1]][2]  %>% strsplit(.,'[+]') %>% unlist()})
+
+link_df=lapply(1:length(L),function(x){
+    expand.grid(L[[x]],R[[x]])
+}) %>% dplyr::bind_rows()
+
+link_df=link_df %>% filter((Var1 %in% df_filter$gene) & (Var2 %in% df_filter$gene))
+
+# reset ligand and receptor order
+df_order=link_df %>% reshape2::dcast(Var1~Var2) %>% data.frame(row.names=1)
+df_order[!is.na(df_order)]=1
+df_order[is.na(df_order)]=0
+df_order=mutate_all(df_order,as.numeric)
+
+p=LinkedPlot(df_filter,link_df,
+             x_col='Niche',
+             y_col='gene',
+             fill_col='signed_combined_rank',
+             size_col='sig_study_ratio',
+             color_column=1,align='center')
+
+p[[1]]=p[[1]]+ggtitle('Ligand')+
+#     scale_fill_gradientn(colours=c(paletteer::paletteer_c("grDevices::RdBu",11)),
+#                          values=scales::rescale(c(max(p[[1]]$data$signed_combined_rank), 0.8,0.7,0.6,0.5,0,
+#                                   -0.5,-0.6,-0.7,-0.8,min(p[[1]]$data$signed_combined_rank) )))+
+    scale_fill_gradientn(colours=c(paletteer::paletteer_c("grDevices::Reds",11)),
+                         values=scales::rescale(c(max(p[[1]]$data$signed_combined_rank), 0.8,0.7,0.6,0.5,0.4,
+                                  0,min(p[[1]]$data$signed_combined_rank) )))+
+    theme(plot.title=element_text(hjust=0.5),axis.text.x=element_text(angle=45,hjust=1))
+p[[3]]=p[[3]]+ggtitle('Receptor')+
+#     scale_fill_gradientn(colours=colorRampPalette(paletteer::paletteer_c("grDevices::RdBu",30))(11),
+#                          values=scales::rescale(c(max(p[[3]]$data$signed_combined_rank), 0.8,0.7,0.6,0.5,0,
+#                                   -0.5,-0.6,-0.7,-0.8,min(p[[3]]$data$signed_combined_rank) )))+
+    scale_fill_gradientn(colours=c(paletteer::paletteer_c("grDevices::Blues",8)),
+                         values=scales::rescale(c(max(p[[3]]$data$signed_combined_rank),0.8,0.7,0.6,0.5,0.4,
+                                  0,min(p[[3]]$data$signed_combined_rank) )))+
+    theme(plot.title=element_text(hjust=0.5),axis.text.x=element_text(angle=45,hjust=1))
+
 p
